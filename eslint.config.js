1766947dd8b14f6946b620773b2399bc75590e34
Matthew Biswas <matthewoncloud@gmail.com>
2026-09-13 20:04:26 +1200

    Comment out ESLint config export
    Disable the project's ESLint configuration by commenting out the default export in eslint.config.js. The original config is retained in-place as commented code so it can be easily restored later.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest"
-    },
-    rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error"
-    }
-  }
-];
+// export default [
+//   {
+//     files: ["**/*.js"],
+//     languageOptions: {
+//       ecmaVersion: "latest"
+//     },
+//     rules: {
+//       "no-unused-vars": "error",
+//       "no-undef": "error"
+//     }
+//   }
+// ];
